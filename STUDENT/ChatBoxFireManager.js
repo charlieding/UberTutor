a865092dea9025911a454fb4c8948ad0c55a1d72
wding <wei.ding@emerson.com>
2016-01-09 13:33:38 -0600

    fixed chatbox race condition

--- STUDENT/ChatBoxFireManager.js
+++ STUDENT/ChatBoxFireManager.js
@@ -27,9 +27,7 @@ var ChatBoxUti={
   },  
 };
 
-var ChatBoxFireManager=function(){
-
-     function GenChatBoxInfo(){
+function GenChatBoxInfo(){
         var usrsObj=null;
         var ret={};
 
@@ -72,11 +70,17 @@ var ChatBoxFireManager=function(){
           if(ret.sortedUidArr.length==0) return alert("ret.sortedUidArr null");
           return ret.sortedUidArr[MsgSnderIdIndx];
         };
-     };//////////////
+     };/////////////
 
-      var chatboxInfo=new GenChatBoxInfo();
+
+var ChatBoxFireManager=function(){
+
+      //var chatboxInfo=new GenChatBoxInfo();
 
       var chatRef = new Firebase("https://ubertutoralpha.firebaseio.com/chat/"); 
+      var usersObj=null;//global
+
+      var chatRooms={};
 
       var chatMsgAddRefs = {};  
       var chatNofiyChangeRefs = {};  
@@ -101,11 +105,10 @@ var ChatBoxFireManager=function(){
         }
       };
       function FireUsers(){
-          if(null===chatboxInfo.getUsersObj() ){
+          if(null===usersObj){
             var usrsRef = new Firebase("https://ubertutoralpha.firebaseio.com/users/");
             usrsRef.once("value",function(snapshot){
-                var usrsObj=snapshot.val();
-                chatboxInfo.setUsersObj(usrsObj);
+                usersObj=snapshot.val();//one time global.
                 once_users();
             });              
           }else{
@@ -113,58 +116,50 @@ var ChatBoxFireManager=function(){
           }
       };
       function once_users(){
-        chatboxInfo.UpdateInfo();
-
-
-        ///////////////////////////////////////////////////////////////
-        //this ui should be handled by html client users.
-        // $("#boxtitle").text(chatboxInfo.getTargetName())
-        //               .attr("chatuid",chatboxInfo.data().sortedChatUid);
-        ////////////////////////////////////////////////////////////////
-
-        FireChatMsgs(chatboxInfo);
+        if(null==usersObj) return alert("usersObj is null");
+        //chatboxInfo.UpdateInfo();
+        $.each(chatRooms,function(chatuid,room){
+            room.Info.setUsersObj(usersObj);
+            FireChatRoom(chatuid);
+        });
       };
-      function FireChatMsgs(chatboxInfo){
+      function FireChatRoom(chatuid){
+        if(undefined === chatRooms[chatuid]){
+          return alert(chatuid+" is not added into rooms yet");
+        }
 
-        //DisconnectChat();
-        //Sort which id is first to generate unique ID
-        var sortedChatUid = chatboxInfo.data().sortedChatUid;
-        console.log("sortedChatUid",sortedChatUid);
-
-
-
-
-        if(chatMsgAddRefs[sortedChatUid]){
-          //var msgsRef = new Firebase("https://ubertutoralpha.firebaseio.com/chat/"+sortedChatUid);
-          chatRef.child(sortedChatUid).child("utc").once("value",on_child_value_msg);
-          return;
+        if(chatMsgAddRefs[chatuid]){
+              if(options.currentChatUid===chatUid){
+                  chatRef.child(chatuid).child("utc").once("value",on_child_value_msg);
+              }                         
+            return;
         };
 
         //load messages via child added
-        var chatBind = chatRef.child(sortedChatUid).child("utc").on("child_added",on_child_added_msg);
-        chatMsgAddRefs[sortedChatUid]=chatBind;
+        var chatBind = chatRef.child(chatuid).child("utc").on("child_added",on_child_added_msg);
+        chatMsgAddRefs[chatuid]=chatBind;
 
         //stats 
+        var chatboxInfo=chatRooms[chatuid].Info;
         var ownerIdIndx=""+chatboxInfo.data().ownerIdIndx;
-        var spath="./"+sortedChatUid+"/"+chatStats+"/"+ownerIdIndx;
+        var spath="./"+chatuid+"/"+chatStats+"/"+ownerIdIndx;
         console.log(spath);
-        chatNofiyChangeRefs[sortedChatUid]=chatRef.child(sortedChatUid).child(chatStats).on("child_changed",child_changed_msg_stats);
-        chatNofiyAddRefs   [sortedChatUid]=chatRef.child(sortedChatUid).child(chatStats).on("child_added",child_changed_msg_stats);
 
-
-      };
+        chatNofiyChangeRefs[chatuid]=chatRef.child(chatuid).child(chatStats).on("child_changed",child_changed_msg_stats);
+        chatNofiyAddRefs   [chatuid]=chatRef.child(chatuid).child(chatStats).on("child_added",child_changed_msg_stats);        
+      }
 
       function child_changed_msg_stats(snapshot){
         var chatuid=snapshot.ref().parent().parent().key();
         var uidIdx=snapshot.key();
         var count=snapshot.val();
         console.log('child_changed_msg_stats', chatuid,uidIdx,count);
-
+        var chatboxInfo=chatRooms[chatuid].Info;
         var ownerIdIndx=""+chatboxInfo.data().ownerIdIndx;
         if(uidIdx===ownerIdIndx){
           console.log("its count is for me.");
-          if(chatboxInfo.notifyStats){
-            chatboxInfo.notifyStats(chatuid, count);
+          if(notifyStats){
+            notifyStats(chatuid, count);
           }
         };
       };
@@ -186,19 +181,31 @@ var ChatBoxFireManager=function(){
         msgAdded2page(chatUid, utc,msgObj,true);  
       };
       function msgAdded2page(chatUid, utc, msgObj, bScroolToView){
-        console.log("key="+utc,msgObj);
+        console.log("key="+utc,msgObj, options);
+
+        if(options){
+              if( options.initChatBoxFunc ){
+                  options.initChatBoxFunc=options.initChatBoxFunc(chatUid);
+              }
+              if(options.currentChatUid!=chatUid){
+                  return;
+              }          
+        }
+
+
+        var chatboxInfo=chatRooms[chatUid].Info;
 
         var snderIdIndx=msgObj.ownerIdIndx;
         var boxsides=chatboxInfo.getSides(snderIdIndx);
-        var snder=chatboxInfo.getUser(snderIdIndx);
+        var snder   =chatboxInfo.getUser(snderIdIndx);
         var snderUid=chatboxInfo.getUid(snderIdIndx);
 
         var localTime = moment.utc(utc).toDate();
         var datetime  = moment(localTime).format("MMM D hh:mm a"); 
 
          
-        if(chatboxInfo.on_msg2chatbox){
-           chatboxInfo.on_msg2chatbox(chatUid, datetime, msgObj, snder, boxsides,bScroolToView);
+        if(on_msg2chatbox){
+           on_msg2chatbox(chatUid, datetime, msgObj, snder, boxsides,bScroolToView);
         }
         return;
       };
@@ -210,7 +217,34 @@ var ChatBoxFireManager=function(){
       }    
 
 
+
+
+      //api bind to a button.
+      this.AddChatRoom=function(tutorID, studentID, currentUserID){
+        var ret = ChatBoxUti.GenSortedChatUID(tutorID, studentID);
+        if(chatRooms[ret.sortedChatUid]!=undefined){
+          return ret.sortedChatUid;
+        }
+        chatRooms[ret.sortedChatUid]={};
+        chatRooms[ret.sortedChatUid].Info=new GenChatBoxInfo();
+        chatRooms[ret.sortedChatUid].Info.Init(tutorID, studentID, currentUserID);
+        return ret.sortedChatUid;
+      };
+   
+      //api bind to a button.
+      var options=null;
+      this.FireChatRooms=function(opts){
+        if(opts){
+          options={};
+          options.currentChatUid=opts.currentChatUid;
+          options.initChatBoxFunc=opts.initChatBoxFunc;//avoid race         
+        };
+
+        FireUsers();
+      };      
+
       this.SendMsg=function(currChatuid, msg){
+        var chatboxInfo=chatRooms[currChatuid].Info;
         var ownerIdIndx=""+chatboxInfo.data().ownerIdIndx;
         var targetIdIndx=""+chatboxInfo.data().targetIdIndx;
 
@@ -219,7 +253,6 @@ var ChatBoxFireManager=function(){
 
         chatRef.child(currChatuid+"/utc/"+utc).set({msg:msg, ownerIdIndx:ownerIdIndx});
 
-
         //stats target incremental.
         chatRef.child(currChatuid+"/"+chatStats+"/"+targetIdIndx).transaction(function(count){
           if(count===null){
@@ -227,36 +260,13 @@ var ChatBoxFireManager=function(){
           }
           return count+1;
         });
-
-      };
-
-
-      function setConnect(tutorID, studentID,currentUserID){
-        chatboxInfo.Init(tutorID, studentID,currentUserID);
-        FireUsers();
-        var chatuid = chatboxInfo.data().sortedChatUid;
-        return chatuid;
-      };
-
-
-
-      //api bind to a button.
-      this.AddChatRoom=function(tutorID, studentID,currentUserID){
-        var gcb=new GenChatBoxInfo();
-        gcb.Init(tutorID, studentID,currentUserID);
-        return setConnect(tutorID, studentID,currentUserID);
-      };
-
-      //api bind to a button.
-      this.SetChatRoom=function(tutorID, studentID,currentUserID){
-        return setConnect(tutorID, studentID,currentUserID);
-      };
-      
+      };      
+      var notifyStats=null;
       this.SetNotifications=function(callbackfunc){
-        chatboxInfo.notifyStats=callbackfunc;
+        notifyStats=callbackfunc;
       };
       this.ClearMyStats=function(currChatuid){
-
+        var chatboxInfo=chatRooms[currChatuid].Info;
         var ownerIdIndx=""+chatboxInfo.data().ownerIdIndx;
         chatRef.child(currChatuid+"/"+chatStats+"/"+ownerIdIndx).transaction(function(count){          
           console.log("ClearMyStats");
@@ -265,11 +275,12 @@ var ChatBoxFireManager=function(){
       };
 
       //api bind to a button.
+      var on_msg2chatbox=null;
       this.Set_FireMsg2Chatbox=function(callbackfunc){
-        chatboxInfo.on_msg2chatbox=callbackfunc;
+        on_msg2chatbox=callbackfunc;
       };
 
-      this.GetChatboxInfo=function(){
-        return chatboxInfo;
+      this.GetChatboxInfo=function(chatuid){
+        return chatRooms[chatuid].Info;
       };
 };////////////////////////////////////////////
