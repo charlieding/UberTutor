4c1c6a63e950fdaa0e7fbcb85452e8c9783e3581
Cheng Ding <charliechengding@gmail.com>
2016-02-26 01:41:51 -0800

    Added overlay and text notification on Appointment Request

--- STUDENT/smsMessage.js
+++ STUDENT/smsMessage.js
@@ -1,8 +1,16 @@
-function sendSmsMessage(to, message){
-	var toPhoneNumber = to.replace(/[^0-9.]/g, "")
-	$.post( "http://52.23.157.36/api/v1/SMSMessage", {to: toPhoneNumber,from: "...", message: "AsapTutor: "+message })
-      .done(function( data ) {
-        console.log( "Data Loaded: ", data );
-        console.log(data.success);
-      });
+function sendSmsMessage(userId, message){
+	console.log("Attempting to send SMS Message to "+ userId);
+	if(userId){
+		(new Firebase("https://ubertutoralpha.firebaseio.com/users/"+userId+"/phoneNumber")).once('value', function (phoneNumberSnapShot) {
+			console.log(phoneNumberSnapShot.val());
+			var toPhoneNumber = phoneNumberSnapShot.val().replace(/[^0-9.]/g, "");
+			$.post( "http://52.23.157.36/api/v1/SMSMessage", {to: toPhoneNumber,from: "...", message: "AsapTutor: "+message })
+		      .done(function( data ) {
+		        console.log( "Data Loaded: ", data );
+		        console.log(data.success);
+		      });
+		});
+  	}
 }
+
+//Charles' User Id facebook:1129363767081285
