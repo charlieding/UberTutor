a5dae7814fdab55cfa7fbf48574817b70e6632d3
Cheng Ding <charliechengding@gmail.com>
2016-03-07 22:58:00 -0800

    Fixed Venmo Paypal Mixup

--- AWSCredentials/bitnami/stack/common/lib/xsltConf.sh
+++
@@ -1,7 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/bitnami/nodejsstack-linux-x64/output/common/lib"
-XSLT_LIBS="-lxslt  -L/bitnami/nodejsstack-linux-x64/output/common/lib -lxml2 -lz -liconv -lm -ldl -lm -lrt"
-XSLT_INCLUDEDIR="-I/bitnami/nodejsstack-linux-x64/output/common/include"
-MODULE_VERSION="xslt-1.1.28"
